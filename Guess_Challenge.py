00d29b0f977fad3ab7797a1c9fbebc461bfd5fbe
Yuliia <yulia.bosher@gmail.com>
2023-10-11 09:55:47 +0100

    added input validation

--- Guess_Challenge.py
+++ Guess_Challenge.py
@@ -6,14 +6,22 @@ def guess():
     # Initialise a variable that would generate a random number from 1 to 9
     random_number = randint(1, 9)
     # Initialise a variable for user input that prompts the user to guess a number from 1 to 9 and converts it to integer
-    user_number = int(input("Guess a number from 1 to 9 "))
+    user_number = input("Guess a number from 1 to 9 ")
+    # Validate that the user input is a digit
+    while not user_number.isdigit():
+        print("Please enter an integer")
+        user_number = input("Guess a number from 1 to 9 ")
+        if user_number.isdigit():
+            break
+    # Convert user input from string to integer
+    converted_number = int(user_number)
     # Compare the randomly generated number and the user input and output the result
-    if random_number > user_number:
-        print(f"The number {user_number} is lower than the random number")
-    elif random_number < user_number:
-        print(f"The number {user_number} is higher than the random number")
+    if random_number > converted_number:
+        print(f"The number {converted_number} is lower than the random number")
+    elif random_number < converted_number:
+        print(f"The number {converted_number} is higher than the random number")
     else:
-        print(f"The number {user_number} is the correct answer!")
+        print(f"The number {converted_number} is the correct answer!")
 
 
 guess()
